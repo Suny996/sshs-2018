0019d3b43581242957af70d3375118cb7cc50e4e
mail.suny@qq.com <Suny2008>
2018-06-25 17:00:53 +0800

    init1

--- sshs-core/src/main/java/com/sshs/core/customise/mapper/CustomiseMapper.java
+++ sshs-core/src/main/java/com/sshs/core/customise/mapper/CustomiseMapper.java
@@ -7,34 +7,41 @@ import java.util.List;
 
 /**
  * 代码生成mapper接口
- * 
+ *
  * @author Suny
  * @date 2017-10-15
  */
 public interface CustomiseMapper extends Mapper<Customise> {
-	/**
-	 * 新增
-	 * 
-	 * @param customise
-	 * @return
-	 */
-	int save(Customise customise);
+    /**
+     * 新增
+     *
+     * @param customise
+     * @return
+     */
+    int save(Customise customise);
 
 
+    /**
+     * 根据主键删除
+     *
+     * @param customise
+     * @return
+     */
+    int delete(Customise customise);
 
-	/**
-	 * 删除
-	 * 
-	 * @param customise
-	 * @return
-	 */
-	int delete(Customise customise);
+    /**
+     * 根据名称删除
+     *
+     * @param customise
+     * @return
+     */
+    int deleteByCustomiseName(Customise customise);
 
-	/**
-	 * 根据主键查询
-	 * 
-	 * @param customise
-	 * @return
-	 */
-	List<Customise> getCustomises(Customise customise);
+    /**
+     * 根据主键查询
+     *
+     * @param customise
+     * @return
+     */
+    List<Customise> getCustomises(Customise customise);
 }
--- sshs-core/src/main/java/com/sshs/core/customise/router/CustomiseRouter.java
+++ sshs-core/src/main/java/com/sshs/core/customise/router/CustomiseRouter.java
@@ -3,13 +3,12 @@ package com.sshs.core.customise.router;
 import com.sshs.core.customise.handler.CustomiseHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 
 @Configuration
 public class CustomiseRouter {
@@ -19,12 +18,9 @@ public class CustomiseRouter {
                 .nest(RequestPredicates.GET("/customise").nest(RequestPredicates.GET("/get/{id}")
                                 .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
                         handler::hello);*/
-      return  RouterFunctions.nest(
-                path("/customise"),
-              RouterFunctions.nest(accept(APPLICATION_JSON),
-                      RouterFunctions.route(GET("/{id}"), customiseHandler::getCustomise)
-                                .andRoute(method(HttpMethod.GET), customiseHandler::listCustomises)
-                ).andRoute(POST("/").and(contentType(APPLICATION_JSON)), customiseHandler::addCustomise)
-        );
+        return nest(
+                path("/customise"), //nest(accept(MediaType.APPLICATION_JSON),
+                RouterFunctions.route(GET("/{id}"), customiseHandler::getCustomise)
+                        .andRoute(POST("/"), customiseHandler::addCustomise).andRoute(DELETE("/"), customiseHandler::delCustomise));
     }
 }
